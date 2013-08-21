8326d99ef03c839494b379254d987f0cedc7c885
Dan Tracy <dant@aweber.com>
2013-08-21 16:21:20 -0400

    supervisor_test: Add test cookbook for use with minitest

--- Berksfile
+++ Berksfile
@@ -5,4 +5,6 @@ metadata
 group :integration do
   cookbook "apt"
   cookbook "yum"
+  cookbook "minitest-handler"
+  cookbook "supervisor_test", :path => "./test/cookbooks/supervisor_test"
 end
---
+++ test/cookbooks/supervisor_test/metadata.rb
@@ -0,0 +1,9 @@
+name             'supervisor_test'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures supervisor_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'supervisor'
