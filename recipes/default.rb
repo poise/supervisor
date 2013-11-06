da28f83e18ba2c21741a60ee232db4a98e4e745e
Gonçalo Silva and Will Hamilton <pair+gsilva+will@pivotallabs.com>
2013-11-06 17:09:26 +0000

    Make the initial service actions (e.g., enable, start) attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -81,7 +81,7 @@ when "debian", "ubuntu"
   end
 
   service "supervisor" do
-    action [:enable, :start]
+    action node['supervisor']['initial_service_actions']
   end
 when "smartos"
   directory "/opt/local/share/smf/supervisord" do
