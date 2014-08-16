2d046282218528d525d4bbd03174851f420bba3c
Noah Kantrowitz <noah@coderanger.net>
2014-08-16 13:42:34 -0700

    Bump version for release.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name              "supervisor"
 maintainer        "Noah Kantrowitz"
-maintainer_email  "noah@opscode.com"
+maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs supervisor and provides resources to configure services"
-version           "0.4.11"
+version           "0.4.12"
 
 recipe "supervisor", "Installs and configures supervisord"
 
