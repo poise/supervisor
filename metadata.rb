6af666252f161faab4c115a615f234feeb8ee24d
Jeff Hull <jsh2134@users.noreply.github.com>
2015-07-07 12:32:29 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs supervisor and provides resources to configure services"
-version           "0.4.13"
+version           "0.4.12"
 
 recipe "supervisor", "Installs and configures supervisord"
 
